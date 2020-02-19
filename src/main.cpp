c712dadbd2ba8b257a90be8f8546e3c034c83782
Przemek Talarek <przemyslaw.talarek@kinguin.io>
2020-02-19 21:27:59 +0100

    send data only when changed

--- src/main.cpp
+++ src/main.cpp
@@ -13,16 +13,24 @@
 
 #define COMPARE_TEMP 1 // Send temperature only if changed? 1 = Yes 0 = No
 #define ONE_WIRE_BUS 8 // Pin where dallase sensor is connected 
-#define SLEEP_TIME_MS 5000 // Sleep time between reads (in milliseconds)
+#define SLEEP_TIME_MS 1000 // Sleep time between reads (in milliseconds)
+
 #define HOT_WATER_INTERRUPT_PIN 2
 #define COLD_WATER_INTERRUPT_PIN 3
+#define HOT_WATER_METER_SENSOR_ID TEMP_DISCOVERY_MAX_ATTACHED_DS18B20 + 1
+#define COLD_WATER_METER_SENSOR_ID TEMP_DISCOVERY_MAX_ATTACHED_DS18B20 + 2
+
+// Indicies for values stored in lastValues array
+#define SV_HOT_WATER_VOLUME TEMP_DISCOVERY_MAX_ATTACHED_DS18B20 + 1
+#define SV_HOT_WATER_FLOW TEMP_DISCOVERY_MAX_ATTACHED_DS18B20 + 2
+#define SV_COLD_WATER_VOLUME TEMP_DISCOVERY_MAX_ATTACHED_DS18B20 + 3
+#define SV_COLD_WATER_FLOW TEMP_DISCOVERY_MAX_ATTACHED_DS18B20 + 4
 
 OneWire oneWire(ONE_WIRE_BUS); 
 DallasTemperature tempSensors(&oneWire);
 TempSensorDiscovery tempSensorDiscovery(&tempSensors);
 
 long lastSendTime = 0;
-float lastTemperature[TEMP_DISCOVERY_MAX_ATTACHED_DS18B20];
 bool receivedConfig = false;
 bool metric = true;
 // Initialize temperature message
@@ -30,19 +38,24 @@ MyMessage temperatureMsg(0, V_TEMP);
 MyMessage waterFlowMsg(0, V_FLOW);
 MyMessage waterVolumeMsg(0, V_VOLUME);
 
-uint8_t sensorsIndicies[TEMP_DISCOVERY_MAX_ATTACHED_DS18B20];
+uint8_t tempSensorsIndicies[TEMP_DISCOVERY_MAX_ATTACHED_DS18B20];
 uint8_t tempSensorsCount;
 
 volatile long hotWaterCounter;
 volatile long coldWaterCounter;
 
-float hotWaterTotalVolumeLiters = 0.0;
-float coldWaterTotalVolumeLiters = 0.0;
+float hotWaterTotalVolumeLiters = 0.000;
+float coldWaterTotalVolumeLiters = 0.000;
+
+float lastValues[25];
 
 void hotWaterISR();
 void coldWaterISR();
 float getWaterVolume(long sensorPulses);
-float getWaterFlow(long sensorPulses, long inTime);
+float getWaterFlow(float flowedVolume, long inTime);
+bool checkIfSensorValueChanged(uint8_t sensorID, float currentValue);
+void setSensorLastValue(uint8_t sensorID, float value);
+float getSensorLastValue(uint8_t sensorID);
 
 void before()
 {
@@ -52,9 +65,10 @@ void before()
 void setup()  
 { 
    Serial.begin(MY_BAUD_RATE);
+
    // requestTemperatures() will not block current thread
    tempSensors.setCheckForConversion(true);
-   tempSensorsCount = tempSensorDiscovery.discover(sensorsIndicies);   
+   tempSensorsCount = tempSensorDiscovery.discover(tempSensorsIndicies);   
 
    // Set interrupts for water flow sensors
    attachInterrupt(digitalPinToInterrupt(HOT_WATER_INTERRUPT_PIN), hotWaterISR, FALLING);
@@ -67,12 +81,12 @@ void presentation() {
 
   // Send initial temp sensor
   for (uint8_t i=0; i<tempSensorsCount; i++) {
-     present(sensorsIndicies[i], S_TEMP);
+     present(tempSensorsIndicies[i], S_TEMP);
   }
   // Hot water flow meter
-  present(20, S_WATER);
+  present(HOT_WATER_METER_SENSOR_ID, S_WATER);
   // Cold water flow meter
-  present(21, S_WATER);
+  present(COLD_WATER_METER_SENSOR_ID, S_WATER);
 }
 
 void loop()     
@@ -80,8 +94,9 @@ void loop()
   // Fetch temperatures from Dallas sensors
   tempSensors.requestTemperatures();
   unsigned long realEplasedTime = millis() - lastSendTime;
-  if (realEplasedTime > SLEEP_TIME_MS) {
-    
+  
+  if (realEplasedTime > SLEEP_TIME_MS) {    
+
     lastSendTime = millis();
 
     long hotWaterImpulses, coldWaterImpulses;
@@ -94,25 +109,42 @@ void loop()
       coldWaterCounter = 0;      
     }
 
-    hotWaterTotalVolumeLiters += getWaterVolume(hotWaterImpulses);
-    send(waterVolumeMsg.setSensor(20).set(hotWaterTotalVolumeLiters, 1));
-    send(waterFlowMsg.setSensor(20).set(getWaterFlow(hotWaterImpulses, realEplasedTime), 2));
+    hotWaterTotalVolumeLiters = hotWaterTotalVolumeLiters + getWaterVolume(hotWaterImpulses);
+    float hotWaterFlow = getWaterFlow(hotWaterTotalVolumeLiters - getSensorLastValue(SV_HOT_WATER_VOLUME), realEplasedTime);
+
+    if (checkIfSensorValueChanged(SV_HOT_WATER_VOLUME, hotWaterTotalVolumeLiters)) {
+      send(waterVolumeMsg.setSensor(HOT_WATER_METER_SENSOR_ID).set(hotWaterTotalVolumeLiters, 3));
+      setSensorLastValue(SV_HOT_WATER_VOLUME, hotWaterTotalVolumeLiters);
+    }
+    if (checkIfSensorValueChanged(SV_HOT_WATER_FLOW, hotWaterFlow)) {
+      send(waterFlowMsg.setSensor(HOT_WATER_METER_SENSOR_ID).set(hotWaterFlow, 2));
+      setSensorLastValue(SV_HOT_WATER_FLOW, hotWaterFlow);
+    }
+
     
-    coldWaterTotalVolumeLiters += getWaterVolume(coldWaterImpulses);
-    send(waterVolumeMsg.setSensor(21).set(coldWaterTotalVolumeLiters, 1));
-    send(waterFlowMsg.setSensor(21).set(getWaterFlow(coldWaterImpulses, realEplasedTime), 2));
+    coldWaterTotalVolumeLiters = coldWaterTotalVolumeLiters + getWaterVolume(coldWaterImpulses);
+    float coldWaterFlow = getWaterFlow(coldWaterTotalVolumeLiters - getSensorLastValue(SV_COLD_WATER_VOLUME), realEplasedTime);
 
-
+    if (checkIfSensorValueChanged(SV_COLD_WATER_VOLUME, coldWaterTotalVolumeLiters)) {
+      send(waterVolumeMsg.setSensor(COLD_WATER_METER_SENSOR_ID).set(coldWaterTotalVolumeLiters, 3));
+      setSensorLastValue(SV_COLD_WATER_VOLUME, coldWaterTotalVolumeLiters);
+    }
+    if (checkIfSensorValueChanged(SV_COLD_WATER_FLOW, coldWaterFlow)) {
+      send(waterFlowMsg.setSensor(COLD_WATER_METER_SENSOR_ID).set(coldWaterFlow, 2));
+      setSensorLastValue(SV_COLD_WATER_FLOW, coldWaterFlow);
+    }
+    
     if (tempSensors.isConversionComplete()) {
       
       for (uint8_t i=0; i<tempSensorsCount; i++) {
         DeviceAddress addr;
-        tempSensorDiscovery.loadSensorAddressByIndex(addr, sensorsIndicies[i]);
+        tempSensorDiscovery.loadSensorAddressByIndex(addr, tempSensorsIndicies[i]);
 
         if (tempSensors.isConnected(addr)) {
           // Fetch and round temperature to one decimal
           float temperature = static_cast<float>(static_cast<int>((getControllerConfig().isMetric?tempSensors.getTempCByIndex(i):tempSensors.getTempFByIndex(i)) * 10.)) / 10.;
-          if (temperature != -127.00 && temperature != 85.00) {
+          if (temperature != -127.00 && temperature != 85.00 && checkIfSensorValueChanged(i, temperature)) {
+            setSensorLastValue(i, temperature);
             send(temperatureMsg.setSensor(i).set(temperature, 1));
             continue;
           }            
@@ -121,7 +153,6 @@ void loop()
     }
   }
 }
-
 
 
 void hotWaterISR() {
@@ -136,6 +167,18 @@ float getWaterVolume(long sensorPulses) {
   return float(sensorPulses) / 288.0;
 }
 
-float getWaterFlow(long sensorPulses, long inTime) {
-  return float(sensorPulses) * (60000.0 / float(inTime)) / (4.8 * 60.0);
+float getWaterFlow(float flowedVolume, long inTime) {
+  return flowedVolume * (60000.0 / float(inTime));
+}
+
+bool checkIfSensorValueChanged(uint8_t sensorID, float currentValue) {
+  return abs(lastValues[sensorID] - currentValue) > 0.000001;
+}
+
+void setSensorLastValue(uint8_t sensorID, float value) {
+  lastValues[sensorID] = value;
+}
+
+float getSensorLastValue(uint8_t sensorID) {
+  return lastValues[sensorID];
 }
